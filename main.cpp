64d9d068130731b91c48c9442277fca16b77d605
onisken <aniskinartyom2002@gmail.com>
2022-05-01 14:03:08 +0300

    Final

--- main.cpp
+++ main.cpp
@@ -3,46 +3,46 @@
 
 int main() {
     std::cout << "Creation of fractions:" << std::endl;
-    CFraction a;
-    CFraction b(1);
-    CFraction c(1, 2);
-    std::cout << "a = " << a.getNumerator() << "/" << a.getDenominator() << std::endl;
-    std::cout << "b = " << b.getNumerator() << "/" << b.getDenominator() << std::endl;
-    std::cout << "c = " << c.getNumerator() << "/" << c.getDenominator() << std::endl;
+    CFraction first;
+    CFraction second(1);
+    CFraction third(1, 2);
+    std::cout << "first = " << first.getNumerator() << "/" << first.getDenominator() << std::endl;
+    std::cout << "second = " << second.getNumerator() << "/" << second.getDenominator() << std::endl;
+    std::cout << "third = " << third.getNumerator() << "/" << third.getDenominator() << std::endl;
     std::cout << std::endl;
 
     std::cout << "Changing fractions:" << std::endl;
-    a = c;
-    b(5);
-    c(2, 3);
-    std::cout << "a = " << a.getNumerator() << "/" << a.getDenominator() << std::endl;
-    std::cout << "b = " << b.getNumerator() << "/" << b.getDenominator() << std::endl;
-    std::cout << "c = " << c.getNumerator() << "/" << c.getDenominator() << std::endl;
+    first = third;
+    second(5);
+    third(2, 3);
+    std::cout << "first = " << first.getNumerator() << "/" << first.getDenominator() << std::endl;
+    std::cout << "second = " << second.getNumerator() << "/" << second.getDenominator() << std::endl;
+    std::cout << "third = " << third.getNumerator() << "/" << third.getDenominator() << std::endl;
     std::cout << std::endl;
 
     std::cout << "Actions with fractions:" << std::endl;
     CFraction temp;
-    temp = a + b;
-    std::cout << "temp = a + b = " << a.getNumerator() << "/" << a.getDenominator() << " + ";
-    std::cout << b.getNumerator() << "/" << b.getDenominator() << " = ";
+    temp = first + second;
+    std::cout << "temp = first + second = " << first.getNumerator() << "/" << first.getDenominator() << " + ";
+    std::cout << second.getNumerator() << "/" << second.getDenominator() << " = ";
     std::cout << temp.getNumerator() << "/" << temp.getDenominator() << std::endl;
 
-    temp = b * c;
-    std::cout << "temp = b * c = " << b.getNumerator() << "/" << b.getDenominator() << " * ";
-    std::cout << c.getNumerator() << "/" << c.getDenominator() << " = ";
+    temp = second * third;
+    std::cout << "temp = second * third = " << second.getNumerator() << "/" << second.getDenominator() << " * ";
+    std::cout << third.getNumerator() << "/" << third.getDenominator() << " = ";
     std::cout << temp.getNumerator() << "/" << temp.getDenominator() << std::endl;
 
-    temp = c * 7;
-    std::cout << "temp = c * 7 = " << c.getNumerator() << "/" << c.getDenominator() << " * " << " 7 = ";
+    temp = third * 7;
+    std::cout << "temp = third * 7 = " << third.getNumerator() << "/" << third.getDenominator() << " * " << " 7 = ";
     std::cout << temp.getNumerator() << "/" << temp.getDenominator() << std::endl;
 
-    std::cout << "a++ = " << a.getNumerator() << "/" << a.getDenominator() << " ++ = ";
-    a = a++;
-    std::cout << a.getNumerator() << "/" << a.getDenominator() << std::endl;
+    std::cout << "first++ = " << first.getNumerator() << "/" << first.getDenominator() << " ++ = ";
+    first = first++;
+    std::cout << first.getNumerator() << "/" << first.getDenominator() << std::endl;
 
-    std::cout << "temp += a++ => " << temp.getNumerator() << "/" << temp.getDenominator() << " += ";
-    std::cout << a++.getNumerator() << "/" << a++.getDenominator() << " => ";
-    temp += a++;
+    std::cout << "temp += first++ => " << temp.getNumerator() << "/" << temp.getDenominator() << " += ";
+    std::cout << first++.getNumerator() << "/" << first++.getDenominator() << " => ";
+    temp += first++;
     std::cout << temp.getNumerator() << "/" << temp.getDenominator() << std::endl;
 
     std::cout << "temp -= 7 => " << temp.getNumerator() << "/" << temp.getDenominator() << " -= 7 => ";
@@ -54,9 +54,9 @@ int main() {
     std::cout << "temp-- = " << temp--.getNumerator() << "/" << temp--.getDenominator() << std::endl;
     std::cout << "temp-- (double) = " << double(temp--) << std::endl;
 
-    std::cout << "temp = b - a = " << b.getNumerator() << "/" << b.getDenominator() << " - ";
-    std::cout << a.getNumerator() << "/" << a.getDenominator() << " = ";
-    temp = b - a;
+    std::cout << "temp = second - first = " << second.getNumerator() << "/" << second.getDenominator() << " - ";
+    std::cout << first.getNumerator() << "/" << first.getDenominator() << " = ";
+    temp = second - first;
     std::cout << temp.getNumerator() << "/" << temp.getDenominator() << std::endl;
 
     std::cout << "temp - 2 = " << temp.getNumerator() << "/" << temp.getDenominator() << " - 2 = ";
@@ -71,79 +71,79 @@ int main() {
     temp *= 4;
     std::cout << temp.getNumerator() << "/" << temp.getDenominator() << std::endl;
 
-    std::cout << "temp *= c => " << temp.getNumerator() << "/" << temp.getDenominator() << " *= ";
-    std::cout << c.getNumerator() << "/" << c.getDenominator() << " => ";
-    temp *= c;
+    std::cout << "temp *= third => " << temp.getNumerator() << "/" << temp.getDenominator() << " *= ";
+    std::cout << third.getNumerator() << "/" << third.getDenominator() << " => ";
+    temp *= third;
     std::cout << temp.getNumerator() << "/" << temp.getDenominator() << std::endl;
 
-    std::cout << "temp /= a => " << temp.getNumerator() << "/" << temp.getDenominator() << " /= ";
-    std::cout << a.getNumerator() << "/" << a.getDenominator() << " => ";
-    temp /= a;
+    std::cout << "temp /= first => " << temp.getNumerator() << "/" << temp.getDenominator() << " /= ";
+    std::cout << first.getNumerator() << "/" << first.getDenominator() << " => ";
+    temp /= first;
     std::cout << temp.getNumerator() << "/" << temp.getDenominator() << std::endl;
 
-    std::cout << "temp = b / a(2, 5) = " << b.getNumerator() << "/" << b.getDenominator();
-    a(2, 5);
-    temp = b / a;
-    std::cout << " / " << a.getNumerator() << "/" << a.getDenominator() << " = ";
+    std::cout << "temp = second / first(2, 5) = " << second.getNumerator() << "/" << second.getDenominator();
+    first(2, 5);
+    temp = second / first;
+    std::cout << " / " << first.getNumerator() << "/" << first.getDenominator() << " = ";
     std::cout << temp.getNumerator() << "/" << temp.getDenominator() << std::endl;
 
     std::cout << "temp /= 5 => " << temp.getNumerator() << "/" << temp.getDenominator() << " /= 5 => ";
     temp /= 5;
     std::cout << temp.getNumerator() << "/" << temp.getDenominator() << std::endl;
 
-    std::cout << "a(25, 10) = 25/10" << std::endl;
-    a(25,10);
+    std::cout << "first(25, 10) = 25/10" << std::endl;
+    first(25, 10);
 
-    if (a == temp) std::cout << "Now, a == temp;";
+    if (first == temp) std::cout << "Now, first == temp;";
     else std::cout << "== is not working :(";
     std::cout << std::endl << std::endl;
 
-    std::cout << "a > b? (a = " << a.getNumerator() << "/" << a.getDenominator();
-    std::cout << "; b = " << b.getNumerator() << "/" << b.getDenominator() << ")" << std::endl;
-    if (a > b) std::cout << "Yes, it's TRUE";
+    std::cout << "first > second? (first = " << first.getNumerator() << "/" << first.getDenominator();
+    std::cout << "; second = " << second.getNumerator() << "/" << second.getDenominator() << ")" << std::endl;
+    if (first > second) std::cout << "Yes, it's TRUE";
     else std::cout << "No, It's FALSE";
     std::cout << std::endl << std::endl;
 
-    std::cout << "a < b? (a = " << a.getNumerator() << "/" << a.getDenominator();
-    std::cout << "; b = " << b.getNumerator() << "/" << b.getDenominator() << ")" << std::endl;
-    if (a < b) std::cout << "Yes, it's TRUE";
+    std::cout << "first < second? (first = " << first.getNumerator() << "/" << first.getDenominator();
+    std::cout << "; second = " << second.getNumerator() << "/" << second.getDenominator() << ")" << std::endl;
+    if (first < second) std::cout << "Yes, it's TRUE";
     else std::cout << "No, It's FALSE";
     std::cout << std::endl << std::endl;
 
-    std::cout << "a <= b? (a = " << a.getNumerator() << "/" << a.getDenominator();
-    std::cout << "; b = " << b.getNumerator() << "/" << b.getDenominator() << ")" << std::endl;
-    if (a <= b) std::cout << "Yes, it's TRUE";
+    std::cout << "first <= second? (first = " << first.getNumerator() << "/" << first.getDenominator();
+    std::cout << "; second = " << second.getNumerator() << "/" << second.getDenominator() << ")" << std::endl;
+    if (first <= second) std::cout << "Yes, it's TRUE";
     else std::cout << "No, It's FALSE";
     std::cout << std::endl << std::endl;
 
-    std::cout << "a = 10/2" << std::endl;
-    a(10, 2);
-    std::cout << "a < b? (a = " << a.getNumerator() << "/" << a.getDenominator();
-    std::cout << "; b = " << b.getNumerator() << "/" << b.getDenominator() << ")" << std::endl;
-    if (a < b) std::cout << "Yes, it's TRUE";
+    std::cout << "first = 10/2" << std::endl;
+    first(10, 2);
+    std::cout << "first < second? (first = " << first.getNumerator() << "/" << first.getDenominator();
+    std::cout << "; second = " << second.getNumerator() << "/" << second.getDenominator() << ")" << std::endl;
+    if (first < second) std::cout << "Yes, it's TRUE";
     else std::cout << "No, It's FALSE";
     std::cout << std::endl << std::endl;
 
-    std::cout << "a <= b? (a = " << a.getNumerator() << "/" << a.getDenominator();
-    std::cout << "; b = " << b.getNumerator() << "/" << b.getDenominator() << ")" << std::endl;
-    if (a <= b) std::cout << "Yes, it's TRUE";
+    std::cout << "first <= second? (first = " << first.getNumerator() << "/" << first.getDenominator();
+    std::cout << "; second = " << second.getNumerator() << "/" << second.getDenominator() << ")" << std::endl;
+    if (first <= second) std::cout << "Yes, it's TRUE";
     else std::cout << "No, It's FALSE";
     std::cout << std::endl << std::endl;
 
-    std::cout << "a >= b? (a = " << a.getNumerator() << "/" << a.getDenominator();
-    std::cout << "; b = " << b.getNumerator() << "/" << b.getDenominator() << ")" << std::endl;
-    if (a >= b) std::cout << "Yes, it's TRUE";
+    std::cout << "first >= second? (first = " << first.getNumerator() << "/" << first.getDenominator();
+    std::cout << "; second = " << second.getNumerator() << "/" << second.getDenominator() << ")" << std::endl;
+    if (first >= second) std::cout << "Yes, it's TRUE";
     else std::cout << "No, It's FALSE";
     std::cout << std::endl << std::endl;
 
-    std::cout << "a == b? (a = " << a.getNumerator() << "/" << a.getDenominator();
-    std::cout << "; b = " << b.getNumerator() << "/" << b.getDenominator() << ")" << std::endl;
-    if (a == b) std::cout << "Yes, it's TRUE";
+    std::cout << "first == second? (first = " << first.getNumerator() << "/" << first.getDenominator();
+    std::cout << "; second = " << second.getNumerator() << "/" << second.getDenominator() << ")" << std::endl;
+    if (first == second) std::cout << "Yes, it's TRUE";
     else std::cout << "No, It's FALSE";
     std::cout << std::endl << std::endl;
 
-    std::cout << "a != 15/3? (a = " << a.getNumerator() << "/" << a.getDenominator() << ")" << std::endl;
-    if (a != temp(15, 3)) std::cout << "Yes, it's TRUE";
+    std::cout << "first != 15/3? (first = " << first.getNumerator() << "/" << first.getDenominator() << ")" << std::endl;
+    if (first != temp(15, 3)) std::cout << "Yes, it's TRUE";
     else std::cout << "No, It's FALSE";
     std::cout << std::endl << std::endl;
 
